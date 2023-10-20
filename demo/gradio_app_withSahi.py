98a78b87ac62bcba024a2c64c3dfa7bf4b7c9e18
kaynelua <luayinghao95@hotmail.com>
2023-10-20 15:37:59 +0800

    Cleaned up gradio app with sahi and parses image with filepath

--- demo/gradio_app_withSahi.py
+++ demo/gradio_app_withSahi.py
@@ -23,53 +23,17 @@ warnings.filterwarnings("ignore")
 
 import gradio as gr
 
-from groundingdino.models import build_model
-from groundingdino.util.slconfig import SLConfig
-from groundingdino.util.utils import clean_state_dict
-from groundingdino.util.inference import annotate, load_image, predict
-import groundingdino.datasets.transforms as T
-
 from huggingface_hub import hf_hub_download
 
 from sahi_general.script.sahi_general import SahiGeneral
 from groundingdino.util.inference import Model
 
 
-
 # Use this command for evaluate the Grounding DINO model
 config_file = "groundingdino/config/GroundingDINO_SwinB_cfg.py" # For faster inference, can use SwinT backbone config : GroundingDINO_SwinT_OGC.py
 ckpt_repo_id = "ShilongLiu/GroundingDINO"
 ckpt_filenmae = "weights/groundingdino_swinb_cogcoor.pth"  # For faster inference, can use SwinT backbone weights: groundingdino_swint_ogc.pth
-
-def load_model_hf(model_config_path, repo_id, filename, device='cpu'):
-    args = SLConfig.fromfile(model_config_path) 
-    model = build_model(args)
-    args.device = device
-
-    cache_file = hf_hub_download(repo_id=repo_id, filename=filename)
-    checkpoint = torch.load(cache_file, map_location='cpu')
-    log = model.load_state_dict(clean_state_dict(checkpoint['model']), strict=False)
-    print("Model loaded from {} \n => {}".format(cache_file, log))
-    _ = model.eval()
-    return model    
-
-def image_transform_grounding(init_image):
-    transform = T.Compose([
-        T.RandomResize([800], max_size=1333),
-        T.ToTensor(),
-        T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
-    image, _ = transform(init_image, None) # 3, h, w
-    return init_image, image
-
-def image_transform_grounding_for_vis(init_image):
-    transform = T.Compose([
-        T.RandomResize([800], max_size=1333),
-    ])
-    image, _ = transform(init_image, None) # 3, h, w
-    return image
-
-#model = load_model_hf(config_file, ckpt_repo_id, ckpt_filenmae)
+  
 
 def run_grounding(input_image, grounding_caption, box_threshold, text_threshold,
                 sahi_image_threshold, sahi_slice_dim ):
@@ -79,16 +43,16 @@ def run_grounding(input_image, grounding_caption, box_threshold, text_threshold,
                            sahi_image_width_threshold=sahi_image_threshold,
                            sahi_slice_height=sahi_slice_dim,
                            sahi_slice_width=sahi_slice_dim,
-                           sahi_overlap_height_ratio=0.2,
-                           sahi_overlap_width_ratio=0.2,
-                           sahi_postprocess_type="GREEDYNMM",
+                           sahi_overlap_height_ratio=0.1,
+                           sahi_overlap_width_ratio=0.1,
+                           sahi_postprocess_type="NMS",
                            sahi_postprocess_match_metric="IOS",
-                           sahi_postprocess_match_threshold=0.5,
+                           sahi_postprocess_match_threshold=0.2,
                            sahi_postprocess_class_agnostic=True,
                            full_frame_detection=False)
 
-    init_image = input_image.convert("RGB")
-    original_size = init_image.size
+    init_image = cv2.imread(str(input_image))
+    #original_size = init_image.size
 
     imgArray = np.array(init_image)
     bs = 1
@@ -117,9 +81,7 @@ def run_grounding(input_image, grounding_caption, box_threshold, text_threshold,
         cv2.putText(draw_frame, f'{grounding_caption} ({confidence:.2f})', (l, t - 8), cv2.FONT_HERSHEY_SIMPLEX, fontScale=1,
                     thickness=3, color=(0, 0, 255))
 
-    #boxes, logits, phrases = predict(model, image_tensor, grounding_caption, box_threshold, text_threshold, device=device)
-    #annotated_frame = annotate(image_source=np.asarray(image_pil), boxes=boxes, logits=logits, phrases=phrases)
-    image_with_box = Image.fromarray(draw_frame)
+    image_with_box = Image.fromarray(cv2.cvtColor(draw_frame, cv2.COLOR_BGR2RGB))
 
 
     return image_with_box
@@ -138,7 +100,7 @@ if __name__ == "__main__":
 
         with gr.Row():
             with gr.Column():
-                input_image = gr.Image(source='upload', type="pil")
+                input_image = gr.Image(source='upload', type="filepath")
                 grounding_caption = gr.Textbox(label="Detection Prompt")
                 run_button = gr.Button(label="Run")
                 with gr.Accordion("Advanced options", open=False):
